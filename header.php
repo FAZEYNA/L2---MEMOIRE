d8c6568d13350ee646add219bd5fed5eec7a2216
FAZEYNA <fazeynafaye@gmail.com>
2021-01-09 10:37:05 +0000

    i define my header

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Recherche de mémoire</title>
+        <link rel="stylesheet" href="/COURS/PHP_L2/L2_memoire/public/css/bootstrap.min.css">  <!-- rechercher les chemins-->
+    </head>
+
+    <body class="bg-light">
